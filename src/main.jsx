d134658df7b414295f147718d5053821c24e9318
Frempong <samankwaa.frempong@gmail.com>
2023-06-27 14:22:18 +0000

    Fix provider error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import './styles/index.css';
 import store from './redux/store';
