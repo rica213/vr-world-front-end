db430102d5602d23b0cac43fbcbe920a5b26df6b
Frempong <samankwaa.frempong@gmail.com>
2023-06-27 14:23:29 +0000

    Add Landingpage route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import NoMatch from './pages/NoMatch';
 import './styles/App.css';
+import Landingpage from './pages/Landingpage';
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Homepage />} />
+      <Route exact path="/" element={<Landingpage />} />
+      <Route exact path="/home" element={<Homepage />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   );
