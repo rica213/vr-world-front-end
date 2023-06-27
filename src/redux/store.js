872c37c47b7340fa0b2de7c21a0ba685bac74025
rica213 <25f5k252l@mozmail.com>
2023-06-27 14:47:06 +0300

    Add reservation slice to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import reservationReducer from './features/ReservationSlice';
 
-const store = configureStore(
-  {
-    reducer: {},
+const store = configureStore({
+  reducer: {
+    reservations: reservationReducer,
   },
-);
+});
 
 export default store;
