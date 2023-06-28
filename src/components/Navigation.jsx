6c3e934b5950d32bc1e5e1c5c24d79fdad24afe3
rica213 <25f5k252l@mozmail.com>
2023-06-28 14:07:11 +0300

    Add all links for navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <nav>
+      <menu class="pages-link">
+        <ul>
+          <li>
+            <Link to="#">Studios</Link>
+          </li>
+          <li>
+            <Link to="#">My Reservations</Link>
+          </li>
+          <li>
+            <Link to="#">Add Studios</Link>
+          </li>
+          <li>
+            <Link to="#">Delete Studios</Link>
+          </li>
+        </ul>
+      </menu>
+      <section class="auth-link">
+        <ul>
+          <li>
+            <Link to="#">Log In</Link>
+          </li>
+          <li>
+            <Link to="#">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="#">Log Out</Link>
+          </li>
+        </ul>
+      </section>
+    </nav>
+  );
+};
 
 export default Navigation;
