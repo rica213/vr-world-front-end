2d87b367352988b3775b63c848945775722108c9
Frempong <samankwaa.frempong@gmail.com>
2023-06-28 23:43:20 +0000

    Fix Linter errors

--- src/pages/ReservationNew.jsx
+++ src/pages/ReservationNew.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 // import { useDispatch } from 'react-redux';
 import '../styles/ReservationNew.css';
 
