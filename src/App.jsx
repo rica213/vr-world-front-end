746abf12047be9691d6647826a6e224b6e287ad7
Frempong <samankwaa.frempong@gmail.com>
2023-06-28 21:56:04 +0000

    Add reservations/new as a route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Homepage from './pages/Homepage';
 import NoMatch from './pages/NoMatch';
 import './styles/App.css';
 import Landingpage from './pages/Landingpage';
+import ReservationNew from './pages/ReservationNew';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Landingpage />} />
       <Route exact path="/home" element={<Homepage />} />
+      <Route exact path="/reservations/new" element={<ReservationNew />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   );
