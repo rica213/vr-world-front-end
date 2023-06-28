d3ef3e0c1412a16e227e97dcd1f21a18b1ae7040
shaaibu <shaaibusuleiman@gamil.com>
2023-06-28 12:56:02 +0100

    Add reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import studioReducer from './features/StudioSlice';
 
 const store = configureStore(
   {
-    reducer: {},
+    reducer: {
+      studios: studioReducer,
+    },
   },
 );
 
