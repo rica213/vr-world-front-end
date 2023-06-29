7ca2e4609529ce0287852cf039edb45365d4a756
rica213 <25f5k252l@mozmail.com>
2023-06-29 17:14:27 +0300

    Add navbar slice into store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reservationReducer from './features/ReservationSlice';
 import AuthenticationReducer from './features/AuthenticationSlice';
+import navReducer from './features/NavbarSlice';
 
 const store = configureStore({
   reducer: {
     auth: AuthenticationReducer,
     reservations: reservationReducer,
+    nav: navReducer,
   },
 });
 
