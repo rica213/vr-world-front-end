5a17d74ed924bc05001ac09613f6f1aaf5029278
rica213 <25f5k252l@mozmail.com>
2023-06-29 22:53:59 +0300

    Fix Linters

--- src/components/AuthRedirect.jsx
+++ src/components/AuthRedirect.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const AuthRedirect = (AuthComponent) => {
-  const AuthRedirectComponent = (props) => {
+  const AuthRedirectComponent = () => {
     const navigate = useNavigate();
     const isLoggedIn = useSelector((state) => state.auth.token !== null);
 
     useEffect(() => {
       if (isLoggedIn) {
-        navigate("/home");
+        navigate('/home');
       }
     }, [isLoggedIn, navigate]);
-  };
 
-  return <AuthComponent {...props} />;
+    return <AuthComponent />;
+  };
+  // result of AuthRedirect HOC
+  return AuthRedirectComponent;
 };
 
 export default AuthRedirect;
