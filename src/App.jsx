a32e57e9bb459d56a9d1d450523c9777af329ba2
rica213 <25f5k252l@mozmail.com>
2023-06-29 17:16:12 +0300

    Add menu icon to the App

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import NoMatch from './pages/NoMatch';
 import './styles/App.css';
 import Landingpage from './pages/Landingpage';
 import Navigation from './components/Navigation';
+import { CgMenuGridO } from "react-icons/cg";
+import { useDispatch } from "react-redux";
+import { toggleNav } from "./redux/features/NavbarSlice";
 
 function App() {
   const location = useLocation();
   const hideNavigation = location.pathname === '/';
+  const dispatch = useDispatch();
+
   return (
     <main>
+      {!hideNavigation && (
+        <CgMenuGridO
+          className="menu-icon"
+          onClick={() => dispatch(toggleNav())}
+        />
+      )}
       {!hideNavigation && <Navigation />}
       <Routes>
         <Route exact path="/" element={<Landingpage />} />
