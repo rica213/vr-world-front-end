4ee721b776fa489e7c4b7f201065c2c6a1922995
ambrose-kibet <ambrosekibet36@gmail.com>
2023-06-30 20:42:10 +0300

    create delete studios page

---
+++ src/pages/RemoveStudios.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchStudios } from '../redux/features/StudioSlice';
+import SingleStudio from '../components/SingleStudio';
+import '../styles/deleteStudios.css';
+
+const RemoveStudios = () => {
+  const { studios } = useSelector((state) => state.studios);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchStudios());
+  }, [dispatch]);
+
+  return (
+    <section className="section-container">
+      <h1 className="text-center">Delete Studios</h1>
+      <div className="delete-studios-container">
+        {studios.map((studio) => (
+          <SingleStudio key={studio.id} details={studio} />
+        ))}
+      </div>
+    </section>
+  );
+};
+export default RemoveStudios;
