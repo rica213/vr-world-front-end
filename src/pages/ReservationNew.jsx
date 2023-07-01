205f65b089f1885f7688cc9e9fdd61a3674d83c9
ambrose-kibet <ambrosekibet36@gmail.com>
2023-07-01 12:59:14 +0300

    fix linters

--- src/pages/ReservationNew.jsx
+++ src/pages/ReservationNew.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import '../styles/ReservationNew.css';
 
 const ReservationNew = () => {
-//   const dispatch = useDispatch;
+  //   const dispatch = useDispatch;
   const [city, setCity] = useState('');
   const [date, setDate] = useState(Date.now());
 
@@ -12,19 +12,32 @@ const ReservationNew = () => {
       <h2>BOOK AN EXPERIENCE</h2>
       <div className="description">
         <p>
-          Choose from over 8 incredible VR studios and unlock a world of limitless possibilities.
-          Dive into undersea adventures, conquer shooting ranges,
-          and explore immersive virtual worlds like never before.
-          Book now and experience the best of VR.
+          Choose from over 8 incredible VR studios and unlock a world of
+          limitless possibilities. Dive into undersea adventures, conquer
+          shooting ranges, and explore immersive virtual worlds like never
+          before. Book now and experience the best of VR.
         </p>
       </div>
       <div className="form-box">
         <form action="submit" className="form">
-          <input type="text" className="city" placeholder="city" value={city} onChange={(e) => setCity(e.target.value)} name="city" />
-          <input type="date" className="date" onChange={(e) => setDate(e.target.value)} value={date} name="date" />
-          <input type="hidden" className="studio" />
-          <input type="hidden" className="username" />
-          <button type="submit" className="submit-btn">Book Now</button>
+          <input
+            type="text"
+            className="city"
+            placeholder="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            name="city"
+          />
+          <input
+            type="date"
+            className="date"
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
+            name="date"
+          />
+          <button type="submit" className="submit-btn">
+            Book Now
+          </button>
         </form>
       </div>
     </div>
